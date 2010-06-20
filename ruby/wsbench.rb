f8123eb46283f4ff9150d87d94a150e9855e6664
Makoto Inoue <makoto@new-bamboo.co.uk>
2010-06-20 23:16:06 +0100

    Added message response time

--- ruby/wsbench.rb
+++ ruby/wsbench.rb
@@ -13,7 +13,6 @@ require 'uri'
 require 'json'
 
 connections =  (ARGV[0] && ARGV[0].to_i) || 250
-p connections
 
 class Connection
   attr_accessor :start_time, :end_time
@@ -26,10 +25,22 @@ class Connection
   end
 end
 
+def show_result(array)
+  oredered_array = array.sort{|a, b| a.time_taken <=> b.time_taken}
+  max = oredered_array.last.time_taken
+  min = oredered_array.first.time_taken
+  sum = array.reduce(0){|total, current| total = total + current.time_taken; total}
+  avg = sum / array.size
+  "sum #{sprintf("%.3f", sum)}, max #{sprintf("%.3f",max)}, min #{sprintf("%.3f",min)}, avg #{sprintf("%.3f", avg)}"
+end
+
+
 uri = URI.parse('ws://localhost')
 uri.port = 8080
 uri.path = "/"
 results = []
+results2 = []
+
 begin_time = Time.now.to_f
 EM.run {
   connections.times do |i|
@@ -40,20 +51,27 @@ EM.run {
       end_time = Time.now.to_f
       result = Connection.new(:connection_id => i, :start_time => start_time, :end_time => end_time)
       results << result
+      # ws.send(JSON.generate({:connection_id => i, :start_time => end_time, :data => "a" * 1000 }))
+      ws.send(JSON.generate({:connection_id => i, :start_time => end_time}))
     }
-    ws.stream{
-      
+    ws.stream{|msg|
+      reply = JSON.parse(msg)
+      # p reply
+      result = Connection.new(
+        :connection_id => reply["connection_id"], 
+        :start_time => reply["start_time"],
+        :end_time => Time.now.to_f
+      )
+      results2 << result
     }
     
   end
-  EventMachine::add_timer(10) {
-    # p results
-    oredered_results = results.sort{|a, b| a.time_taken <=> b.time_taken}
-    max = oredered_results.last.time_taken
-    min = oredered_results.first.time_taken
-    sum = results.reduce(0){|total, current| total = total + current.time_taken; total}
-    avg = sum / results.size
-    p "#{results.size } connections, sum #{sum}, max #{max}, min #{min}, avg #{avg}"
+  EventMachine::add_timer(20) {
+    print "#{results.size}: "
+    print "Connect: #{show_result(results)} :"
+    print "Message: #{show_result(results2)}\n"
+
     EventMachine::stop
   }
 }
+
