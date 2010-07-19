cbcfc0b9a1b0555bed621f67721aeb97ef4d04e8
Makoto Inoue <makoto@new-bamboo.co.uk>
2010-07-20 00:01:00 +0100

    Implemented stats["sum"]

--- go/wsbench/wsbench.go
+++ go/wsbench/wsbench.go
@@ -3,21 +3,66 @@
 */
 package wsbench
 
-type Result struct{
-  time int
+import (
+  "bytes"
+  "fmt"
+  "time"
+  "websocket"
+)
+
+func sum(a []int64) int64 { // returns an int
+    var s int64 = 0
+    for i := 0; i < len(a); i++ {
+        s += a[i]
+    }
+    return s
 }
 
-type WSBench struct{
+type Result struct {
+  time int64
+}
+
+type WSBench struct {
   connections int
-  results []Result
+  target      string
+  results     []Result
+  stats       map[string] int64
 }
 
-func (w *WSBench) Run (){
+func (w *WSBench) Run() {
   w.results = make([]Result, w.connections)
-  for i := 0; i < 2; i++ {
+
+  msg := []byte("hello, world!")
+
+  for i := 0; i < w.connections; i++ {
+    start := time.Nanoseconds()
+    ws, err := websocket.Dial("ws://localhost:12345/echo", "", "http://localhost/")
+    if err != nil {
+      panic("Dial failed: " + err.String())
+    }
+
+    if _, err := ws.Write([]byte(msg)); err != nil {
+      panic("Write failed: " + err.String())
+    }
+
+    var response = make([]byte, 512)
+    fmt.Println(bytes.Equal(response, response))
+    n, err := ws.Read(response)
+
+    if err != nil {
+      panic("Read failed: " + err.String())
+    }
+    if !bytes.Equal(msg, response[0:n]) {
+      panic("Message not the same: " + err.String())
+    }
+    ws.Close()
+    delta := time.Nanoseconds() - start
     // Adding Dummy result for now.
-    w.results[i] = Result{time:2}
+    w.results[i] = Result{time: delta}
   }
+  times := make([]int64, w.connections)
+  for i := range w.results {
+    times[i] = w.results[i].time
+  }
+  w.stats = map[string] int64{"sum":sum(times)}
 }
-
-
--- go/wsbench/wsbench_test.go
+++ go/wsbench/wsbench_test.go
@@ -9,7 +9,7 @@ import (
   // "net"
   "once"
   "websocket"
-	"testing"
+  "testing"
 )
 
 var serverAddr string
@@ -17,50 +17,64 @@ var serverAddr string
 func echoServer(ws *websocket.Conn) { io.Copy(ws, ws) }
 
 func startServer() {
-  http.Handle("/echo", websocket.Handler(echoServer));
-  go http.ListenAndServe(":12345", nil);
+  http.Handle("/echo", websocket.Handler(echoServer))
+  go http.ListenAndServe(":12345", nil)
 }
 
 func TestEcho(t *testing.T) {
   once.Do(startServer)
-	msg := []byte("hello, world!")
-  ws, err := websocket.Dial("ws://localhost:12345/echo", "", "http://localhost/");
+  msg := []byte("hello, world!")
+  ws, err := websocket.Dial("ws://localhost:12345/echo", "", "http://localhost/")
   if err != nil {
-   t.Errorf("WebSocket handshake: %v", err)
+    t.Errorf("WebSocket handshake: %v", err)
   }
   if _, err := ws.Write([]byte(msg)); err != nil {
-   t.Errorf("Write: error %v", err)
+    t.Errorf("Write: error %v", err)
   }
   var response = make([]byte, 512)
   fmt.Println(bytes.Equal(response, response))
-  n, err := ws.Read(response); 
-  
+  n, err := ws.Read(response)
+
   if err != nil {
-   t.Errorf("Read: error %v", err)
+    t.Errorf("Read: error %v", err)
   }
-  
+
   if !bytes.Equal(msg, response[0:n]) {
-   t.Errorf("Echo: expected %q got %q", msg, response)
+    t.Errorf("Echo: expected %q got %q", msg, response)
   }
   ws.Close()
 }
 
 func TestSetClientConnections(t *testing.T) {
- once.Do(startServer)
- wsClients := &WSBench{connections:2}
- if wsClients.connections != 2{
-  t.Errorf("Setting connections error")
- }
+  once.Do(startServer)
+  wsClients := &WSBench{connections: 2, target: "ws://localhost:12345/echo"}
+  if wsClients.connections != 2 {
+    t.Errorf("Setting connections error")
+  }
+  if wsClients.target != "ws://localhost:12345/echo" {
+    t.Errorf("Setting connections error")
+  }
+
 }
 
 func TestRunCreatesMultipleConnections(t *testing.T) {
   once.Do(startServer)
-  wsClients := &WSBench{connections:2}
+  wsClients := &WSBench{connections: 2}
   wsClients.Run()
-  fmt.Printf("A: %v :B", wsClients.results)
+  fmt.Printf("A: %v ", wsClients.results)
   if len(wsClients.results) != 2 {
     t.Errorf("Running WSBench w 2 connections should return 2 results ", wsClients.results, 2)
   }
 }
 
+func TestStatShouldHaveSum(t *testing.T) {
+  once.Do(startServer)
+  wsClients := &WSBench{connections: 3}
+  wsClients.Run()
+  fmt.Printf("A: %v ", wsClients.results)
+  fmt.Printf("A: %v ", wsClients.stats["sum"])
+  if wsClients.stats["sum"] <= 0 {
+    t.Errorf("Stats should have sum", wsClients.stats["sum"], 0)
+  }
+}
 
