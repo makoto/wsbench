206dddfa0fe40ff8f9136e53393cbf01225e977c
Makoto Inoue <makoto@new-bamboo.co.uk>
2010-08-03 23:01:20 +0100

    Added broadcastHandler

--- go/wsbench/wsbench_test.go
+++ go/wsbench/wsbench_test.go
@@ -19,8 +19,58 @@ func echoServer(ws *websocket.Conn) {
   io.Copy(ws, ws) 
 }
 
+var messageChan = make(chan []byte)
+
+type subscription struct {
+    conn      *websocket.Conn
+    subscribe bool
+}
+
+var subscriptionChan = make(chan subscription)
+
+func hub() {
+    conns := make(map[*websocket.Conn]int)
+    for {
+        select {
+        case subscription := <-subscriptionChan:
+            conns[subscription.conn] = 0, subscription.subscribe
+        case message := <-messageChan:
+            for conn, _ := range conns {
+                if _, err := conn.Write(message); err != nil {
+                    conn.Close()
+                }
+            }
+        }
+    }
+}
+
+func broadcastHandler(ws *websocket.Conn) {
+    defer func() {
+        subscriptionChan <- subscription{ws, false}
+        ws.Close()
+        fmt.Printf("Closed \n")
+    }()
+
+    fmt.Printf("Adding to subscription \n")
+
+    subscriptionChan <- subscription{ws, true}
+    fmt.Printf("Added to subscription \n")
+
+    buf := make([]byte, 256)
+    for {
+        n, err := ws.Read(buf)
+        fmt.Printf("Reding message %v \n", n)
+        
+        if err != nil {
+            break
+        }
+        messageChan <- buf[0:n]
+    }
+}
+
 func startServer() {
   http.Handle("/echo", websocket.Handler(echoServer))
+  http.Handle("/broadcast", websocket.Handler(broadcastHandler))
   go http.ListenAndServe(":5555", nil)
 }
 
