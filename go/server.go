61fd8c0c2a05b29e6652b423768b471cff9e7fd0
Makoto Inoue <makoto@new-bamboo.co.uk>
2010-08-04 09:02:25 +0100

    Changed port number

--- go/server.go
+++ go/server.go
@@ -4,16 +4,18 @@ import (
 	"http"
 	"io"
 	"websocket"
+	"fmt"
 )
 
 // Echo the data received on the Web Socket.
 func EchoServer(ws *websocket.Conn) {
+  fmt.Printf("at EchoServer")
 	io.Copy(ws, ws);
 }
 
 func main() {
 	http.Handle("/echo", websocket.Handler(EchoServer));
-	err := http.ListenAndServe(":12345", nil);
+	err := http.ListenAndServe(":5555", nil);
 	if err != nil {
 		panic("ListenAndServe: " + err.String())
 	}
