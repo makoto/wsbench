7934471f103084b4a8e4fc5a25d07040d924aa1f
Makoto Inoue <makoto@new-bamboo.co.uk>
2010-08-08 21:38:09 +0100

    Added echo-server.js

---
+++ node/echo-server.js
@@ -0,0 +1,92 @@
+var sys = require("sys")
+  , fs = require("fs")
+  , path = require("path")
+  , http = require("http")
+  , ws = require('./node-websocket-server/lib/ws');
+
+/*-----------------------------------------------
+  logging:
+-----------------------------------------------*/
+var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+function pad(n) {
+  return n < 10 ? '0' + n.toString(10) : n.toString(10);
+}
+
+function timestamp() {
+  var d = new Date();
+  return [
+    d.getDate(),
+    months[d.getMonth()],
+    [ pad(d.getHours())
+    , pad(d.getMinutes())
+    , pad(d.getSeconds())
+    , (d.getTime() + "").substr( - 4, 4)
+    ].join(':')
+  ].join(' ');
+};
+
+function log(msg) {
+  sys.puts(timestamp() + ' - ' + msg.toString());
+};
+
+function serveFile(req, res){
+  if( req.url.indexOf("favicon") > -1 ){
+    log("HTTP: inbound request, served nothing, (favicon)");
+    
+    res.writeHead(200, {'Content-Type': 'image/x-icon'});
+    res.end("");
+  } else {
+    log("HTTP: inbound request, served client.html");
+    
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream( path.normalize(path.join(__dirname, "client.html")), {
+      'flags': 'r',
+      'encoding': 'binary',
+      'mode': 0666,
+      'bufferSize': 4 * 1024
+    }).addListener("data", function(chunk){
+      res.write(chunk, 'binary');
+    }).addListener("close",function() {
+      res.end();
+    });
+  }
+};
+
+/*-----------------------------------------------
+  Spin up our server:
+-----------------------------------------------*/
+var httpServer = http.createServer(serveFile);
+
+
+var server = ws.createServer({
+  debug: false,
+  version: "draft75"
+}, httpServer);
+
+server.addListener("listening", function(){
+  log("Listening for connections.");
+});
+
+// Handle WebSocket Requests
+server.addListener("connection", function(conn){
+  // log("opened connection: "+conn.id);
+  
+  conn.addListener("message", function(message){
+    log("<"+conn.id+"> "+message);
+    server.send(conn.id, message);
+  });
+});
+
+server.addListener("close", function(conn){
+  log("closed connection: "+conn.id);
+  conn.broadcast("<"+conn.id+"> disconnected");
+});
+
+server.listen(5000);
+// Handle HTTP Requests:
+
+// This will hijack the http server, if the httpserver doesn't 
+// already respond to http.Server#request
+
+// server.addListener("request", serveFile);
